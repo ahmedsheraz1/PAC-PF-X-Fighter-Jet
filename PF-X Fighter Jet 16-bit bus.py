013a1ab1dc5f2c81c2fdc7e7b3898dcb7554d597
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-01 17:25:56 +0500

    Create PF-X Fighter Jet 16-bit bus.py

---
+++ PF-X Fighter Jet 16-bit bus.py
@@ -0,0 +1,7 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+octets = [192, 168, 0, 1]
+'{:02X}{:02X}{:02X}{:02X}'.format(*octets)
+'C0A80001'
+int(_, 16)
+3232235521
